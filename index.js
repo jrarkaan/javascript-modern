4d84302584853224b860f09d6e44c4ac62e7a6ff
JrArkaan <51152071+jrarkaan@users.noreply.github.com>
2020-10-21 23:20:54 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const rincianPembayaran = [
+    {
+        "_id": "5f8fae41950d1513705e67a0",
+        "data": {
+            "rincianBayar": "Peserta Open Perorangan",
+            "hargaSatuan": 175000,
+            "kuantitas": 2,
+            "subtotal": 300000,
+        }
+    },
+    {
+        "_id": "5f8fad2f8bc3aa272ce2ad5e",
+        "data": {
+            "rincianBayar": "Peserta Open beregu",
+            "hargaSatuan": 300000,
+            "kuantitas": 1,
+            "subtotal": 300000,
+        }
+    },
+    {
+       "_id": "5f8fc8252e2abc1ff83d8ec6",
+        "data": {
+            "rincianBayar": "Peserta Festival",
+            "hargaSatuan": 195000,
+            "kuantitas": 1,
+            "subtotal": 195000,
+        }
+    }
+];
+
+const data = rincianPembayaran
+  .map( ({ _id, data })=> {
+    return data.subtotal;
+  })
+  .reduce((acc, curr) => {
+    return acc+curr;
+  })
+
+console.log(data);
